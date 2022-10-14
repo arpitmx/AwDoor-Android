b4cb8877da2e97efc660a4dbf390a0643045840b
arpitmx <arpitmaurya01codechef@gmail.com>
2022-10-15 03:07:07 +0530

    Created onboarding page

--- app/src/main/java/com/ncs/awdoor/User_Details.kt
+++ app/src/main/java/com/ncs/awdoor/User_Details.kt
@@ -3,10 +3,9 @@ package com.ncs.awdoor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class User_Details : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_user_details)
     }
 }
