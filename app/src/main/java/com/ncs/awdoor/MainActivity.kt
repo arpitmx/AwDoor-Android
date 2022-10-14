65be2464897cbdc961620185d04e6af96fd9feb2
arpitmx <arpitmaurya01codechef@gmail.com>
2022-10-15 04:48:23 +0530

    Created details page

--- app/src/main/java/com/ncs/awdoor/MainActivity.kt
+++ app/src/main/java/com/ncs/awdoor/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.ncs.awdoor
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.app.ActivityOptionsCompat
+import androidx.core.view.ViewCompat
+import com.ncs.awdoor.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
+
+    public fun actionSearchFrag(item : View){
+        val intent = Intent(this, User_Details::class.java)
+        val options : ActivityOptionsCompat = ActivityOptionsCompat.makeSceneTransitionAnimation(this,item,
+            ViewCompat.getTransitionName(item).toString()
+        )
+        startActivity(intent,options.toBundle())
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.next.setOnClickListener {
+            actionSearchFrag(binding.next)
+       }
+
 
     }
 }
