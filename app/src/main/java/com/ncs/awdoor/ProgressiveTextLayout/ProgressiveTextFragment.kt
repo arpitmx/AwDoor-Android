e258cd7b9c87495bd772251de21f608f6ba955fe
Adarsh Bajpai <adarshbajpai67@gmail.com>
2022-10-16 01:59:46 +0530

    More work on progressive text inputs

--- app/src/main/java/com/ncs/awdoor/ProgressiveTextLayout/ProgressiveTextFragment.kt
+++ app/src/main/java/com/ncs/awdoor/ProgressiveTextLayout/ProgressiveTextFragment.kt
@@ -3,9 +3,11 @@
 import android.content.Context
 import android.os.Bundle
 import android.text.Editable
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -15,6 +17,7 @@ import com.ncs.awdoor.R
 import com.ncs.awdoor.databinding.FragmentProgressiveTextBinding
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.properties.Delegates
 
  class ProgressiveTextFragment : Fragment() {
 
@@ -25,12 +28,14 @@ import java.util.*
     private lateinit var viewModel: ProgressiveTextViewModel
     private var _binding : FragmentProgressiveTextBinding? = null
     private val binding get()  = _binding!!
+    lateinit var arrayAdapter :ArrayAdapter<String?>
 
      lateinit var date: String
      lateinit var destination : String
      lateinit var nodays : String
-     lateinit var tripType : String
-     lateinit var transport : String
+     var tripType : Int = -1
+     var transport : Int = -1
+     var willing : Int = -1
      lateinit var noOfpeople : String
 
 
@@ -42,20 +47,53 @@ import java.util.*
         _binding = FragmentProgressiveTextBinding.inflate(inflater,container,false)
 
         val languages = resources.getStringArray(R.array.triptype)
-        val arrayAdapter = ArrayAdapter(requireContext(), R.layout.drop_down_item , languages)
+        arrayAdapter = ArrayAdapter(requireContext(), R.layout.drop_down_item , languages)
         val autocompleteTV =binding.autoCompleteTextView
         autocompleteTV.setAdapter(arrayAdapter)
+        autocompleteTV.setOnItemClickListener { adapterView, view, i, l ->
+            tripType = l.toInt()
+            binding.autoCompleteTextView.error = null
+
+            if (tripType == 1){
+                binding.detail45.visibility = View.VISIBLE
+            }else {
+                binding.detail45.visibility = View.GONE
+                tripType = 0
+            }
+        }
 
 
         val transports = resources.getStringArray(R.array.transport)
         val transportAdapter = ArrayAdapter(requireContext(), R.layout.drop_down_item , transports)
         val transportView =binding.transportView
         transportView.setAdapter(transportAdapter)
+        transportView.setOnItemClickListener{
+                adapterView, view, i, l ->
+            transport = l.toInt()
+            binding.transportView.error = null
+
+
+        }
+
 
         val will = resources.getStringArray(R.array.will)
         val willAdapter = ArrayAdapter(requireContext(), R.layout.drop_down_item , will)
         val willView =binding.hotelwill
         willView.setAdapter(willAdapter)
+        willView.setOnItemClickListener{
+            adapterView, view , i, l ->
+            binding.hotelwill.error = null
+            willing = l.toInt()
+
+            if (willing == 0){
+                binding.detail6.visibility = View.VISIBLE
+            }else {
+                binding.detail6.visibility = View.GONE
+                willing = 1
+            }
+
+
+        }
 
         val star = resources.getStringArray(R.array.stars)
         val starAdapter = ArrayAdapter(requireContext(), R.layout.drop_down_item , star)
@@ -127,6 +165,7 @@ import java.util.*
 
 
 
+                    handleGroupType()
 
                 }else {
                     Toast.makeText(requireContext(),"Some field still left", Toast.LENGTH_SHORT).show()
@@ -145,30 +184,60 @@ import java.util.*
 
 
      fun handleGroupType(){
-         if(!binding.autoCompleteTextView.toString().trim().equals("") ){
-             tripType = binding.autoCompleteTextView.toString().trim()
-
-             var group = false
-             if (tripType == "group"){
-                 group = true
-                 binding.detail45.visibility = View.VISIBLE
+         if(tripType!=-1){
+             binding.autoCompleteTextView.error = null
+             if (tripType == 1 && !binding.noofpeople.text.toString().trim().equals("")){
+                 noOfpeople = binding.noofpeople.text.toString().trim()
+                 showTransport(tripType)
+             }else {
+                 showTransport(tripType)
              }
-             if (group && !binding.noofpeople.toString().trim().equals("") || !group){
 
-                 noOfpeople = binding.noofpeople.toString().trim()
-                 binding.b.p3.setImageResource(R.drawable.ic_se)
-                 binding.b.l3.setBackgroundColor(resources.getColor(R.color.primary))
-                 binding.b.p4.setImageResource(R.drawable.ic_do)
-
-                 binding.detail4.visibility = View.VISIBLE
-                 binding.detail3.alpha = 0.5f
-                 binding.detail45.alpha = 0.5f
-
-             }
 
          }else {
-             binding.autoCompleteTextView.error = "Empty"
+             Toast.makeText(requireContext(),"Select Solo or Group trip", Toast.LENGTH_SHORT).show()
          }
+     }
+
+
+
+     fun showTransport(trip : Int){
+         binding.b.p3.setImageResource(R.drawable.ic_se)
+         binding.b.l3.setBackgroundColor(resources.getColor(R.color.primary))
+         binding.b.p4.setImageResource(R.drawable.ic_do)
+         binding.autoCompleteTextView.alpha = 0.5f
+         if (trip == 1){
+            binding.detail45.alpha = 0.5f
+        }
+         binding.detail4.visibility = View.VISIBLE
+
+         if (transport != -1){
+             showHotel()
+         }else {
+             Toast.makeText(requireContext(),"Select transport", Toast.LENGTH_SHORT).show()
+         }
+
+     }
+
+     fun showHotel(){
+         binding.b.p4.setImageResource(R.drawable.ic_se)
+         binding.b.l4.setBackgroundColor(resources.getColor(R.color.primary))
+         binding.b.p5.setImageResource(R.drawable.ic_do)
+         binding.detail4.alpha = 0.5f
+         binding.detail5.visibility = View.VISIBLE
+         if (willing!= -1){
+                if (willing == 0){
+                    showStars(willing)
+                }else {
+                    binding.b.p5.setImageResource(R.drawable.ic_se)
+                }
+         }else {
+             Toast.makeText(requireContext(),"Will you stay in hotel ? ", Toast.LENGTH_SHORT).show()
+         }
+     }
+     fun showStars(willing : Int){
+         binding.detail6.visibility =View.VISIBLE
+
      }
 
      override fun onAttach(context: Context) {
