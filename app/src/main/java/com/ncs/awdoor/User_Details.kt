ac51928535c1a1560cf1267d5cb5494aeef43ddc
arpitmx <arpitmaurya01codechef@gmail.com>
2022-10-15 18:47:12 +0530

    More work on progressive text inputs

--- app/src/main/java/com/ncs/awdoor/User_Details.kt
+++ app/src/main/java/com/ncs/awdoor/User_Details.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.ncs.awdoor.databinding.ActivityUserDetailsBinding
 
 class User_Details : AppCompatActivity() {
@@ -12,6 +13,10 @@ class User_Details : AppCompatActivity() {
     lateinit var sharedPref : SharedPreferences
     lateinit var editor : SharedPreferences.Editor
     lateinit var binding : ActivityUserDetailsBinding
+
+    lateinit var name: String
+    lateinit var state: String
+    lateinit var city: String
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,20 +29,52 @@ class User_Details : AppCompatActivity() {
         setViews()
     }
 
+    fun takeDetails() : Boolean {
+        val valid = false
+        name = binding.name.text.toString().trim()
+        state = binding.state.text.toString().trim()
+        city  = binding.city.text.toString().trim()
+
+        if (name != ""){
+            if (state != ""){
+                if (city != ""){
+                    binding.nameInp.error = ""
+                    binding.cityInp.error = ""
+                    binding.stateInp.error = ""
+                    return true
+                }else{
+                    binding.cityInp.error = "City cant be empty"
+                    return  false
+                }
+            }else {
+                binding.stateInp.error = "State cant be empty"
+                return false
+            }
+        }else {
+            binding.nameInp.error = "Name cant be empty"
+            return false
+        }
+
+    }
+
     fun setViews() : Unit {
 
         binding.begin.setOnClickListener{
 
-            val name = binding.name.text.toString()
-            val state = binding.state.text.toString()
-            val city = binding.city.text.toString()
 
-            editor.putString("username",name)
-            editor.putString("state",state)
-            editor.putString("city",city)
-            editor.commit()
+            if (takeDetails()){
 
-            startActivity(Intent(this, TripDetails::class.java))
+                editor.putString("username",name)
+                editor.putString("state",state)
+                editor.putString("city",city)
+                editor.commit()
+                startActivity(Intent(this, TripDetails::class.java))
+            }else {
+                Toast.makeText(this,"Please fill form", Toast.LENGTH_SHORT).show()
+            }
+
+
+
         }
 
     }
